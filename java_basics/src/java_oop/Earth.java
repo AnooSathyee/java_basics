7190a44b99d4a5b25e44e96a4db9d3c99f85d7c6
Anu <anoosathyee@gmail.com>
2021-07-01 15:40:13 -0500

    added another human

--- java_basics/src/java_oop/Earth.java
+++ java_basics/src/java_oop/Earth.java
@@ -9,6 +9,9 @@ public class Earth {
 		System.out.println("===========================");
 		Human human2 = new Human("Vaish", 16, 67, "brown");
 		human2.speak();
+		System.out.println("===========================");
+		Human human3 = new Human("Anya", 11, 56, "brown");
+		human3.speak();
 	}
 
 }
